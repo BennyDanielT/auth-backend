7be47b921096ee3d863b1845868285e292826922
Benny Daniel <benny28dany@gmail.com>
2022-08-18 15:42:39 -0300

    Working Code for Add

--- app.js
+++ app.js
@@ -1,15 +1,55 @@
-const express = require("express");
+const bcrypt = require('bcrypt');
+const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const dbConnect = require('./db/dbConnect');
+const Inventory = require('./db/storageModel');
+
+// execute database connection
+dbConnect();
 
 // body parser configuration
 app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (request, response, next) => {
-  response.json({ message: "Hey! This is your server response!" });
+app.get('/', (request, response, next) => {
+  response.json({ message: 'Hey! This is your server response!' });
   next();
 });
 
+// add endpoint
+app.post("/add", (request, response) => {
+  
+      // create a new entry instance and collect the data
+      const logEntry = new Inventory({
+        province: request.body.province,
+        region: request.body.region,
+        store: request.body.store,
+        brand: request.body.brand,
+        product: request.body.product,
+        facings: request.body.facings,
+        logDate: request.body.logDate,
+      });
+
+      // save the new entry
+      logEntry
+        .save()
+        // return success if the new entry is added to the database successfully
+        .then((result) => {
+          response.status(201).send({
+            message: "Log Entry Created Successfully",
+            result,
+          });
+        })
+        // catch erroe if the new entry wasn't added successfully to the database
+        .catch((error) => {
+          response.status(500).send({
+            message: "Error creating Log Entry",
+            error,
+          });
+        });
+
+   
+});
 
 module.exports = app;
